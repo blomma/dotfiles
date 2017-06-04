6735c366c597bb8ccab93a99ed52dfeb0f082ecf
Mikael Hultgren <blomma@gmail.com>
2017-06-04 14:11:48 +0200

    Yeah

--- mutt/.offlineimap.py
+++ mutt/.offlineimap.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def imappasswd():
-    args = ["/usr/bin/security", "find-internet-password", "-g", "-a", "blomma@artsoftheinsane.com", "-s", "mail.messagingengine.com", "-r", "imap", "-w"]
+    args = ["/usr/bin/security", "find-internet-password", "-a", "blomma@artsoftheinsane.com", "-s", "mail.messagingengine.com", "-r", "imap", "-w"]
     try:
         return subprocess.check_output(args).strip()
     except subprocess.CalledProcessError:
