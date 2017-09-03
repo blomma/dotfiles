dfe84af2781f5d12ff43803a744e6c8badec6daa
Mikael Hultgren <blomma@gmail.com>
2017-09-03 18:13:43 +0200

    And some more

--- mutt/.offlineimap.py
+++ mutt/.offlineimap.py
@@ -1,6 +1,7 @@
 import subprocess
 import os.path
 
+# Generate password with openssl enc -aes-256-cbc -in password.txt  -out .offlineimap.password
 def imappasswd():
     path = os.path.expanduser('~/.offlineimap.password')
     args = ["openssl", "enc", "-d", "-aes-256-cbc", "-in", path]
--- mutt/README.md
+++
@@ -1,2 +0,0 @@
-echo "<PASSWORD>" > password
-openssl enc -aes-256-cbc -in password  -out .offlineimap.password
