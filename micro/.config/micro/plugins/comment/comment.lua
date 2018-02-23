66f3d1e3defbd3b3968403350d6f0328058efa94
Mikael Hultgren <blomma@gmail.com>
2018-02-23 19:13:49 +0100

    Lots of stuff

--- micro/.config/micro/plugins/comment/comment.lua
+++ micro/.config/micro/plugins/comment/comment.lua
@@ -1,4 +1,4 @@
-VERSION = "1.0.4"
+VERSION = "1.0.6"
 
 ft = {}
 
@@ -31,29 +31,33 @@ function commentLine(lineN)
     local v = CurView()
     local line = v.Buf:Line(lineN)
     local commentType = v.Buf.Settings["commenttype"]
-    local commentRegex = "^%s*" .. commentType:gsub("%*", "%*"):gsub("%-", "%-"):gsub("%.", "%."):gsub("%+", "%+"):gsub("%[", "%["):gsub("%%s", "(.*)")
+    local commentRegex = "^%s*" .. commentType:gsub("%*", "%*"):gsub("%-", "%-"):gsub("%.", "%."):gsub("%+", "%+"):gsub("%]", "%]"):gsub("%[", "%["):gsub("%%s", "(.*)")
+    local sel = -v.Buf.Cursor.CurSelection
+    local curpos = -v.Buf.Cursor.Loc
     local index = string.find(commentType, "%%s") - 1
     if string.match(line, commentRegex) then
         uncommentedLine = string.match(line, commentRegex)
         v.Buf:Replace(Loc(0, lineN), Loc(#line, lineN), GetLeadingWhitespace(line) .. uncommentedLine)
         if v.Buf.Cursor:HasSelection() then
-            if v.Buf.Cursor.CurSelection[1].Y == v.Buf.Cursor.CurSelection[2].Y then
-                v.Buf.Cursor.CurSelection[1].X = v.Buf.Cursor.CurSelection[1].X - index
-                v.Buf.Cursor.CurSelection[2].X = v.Buf.Cursor.CurSelection[2].X - index
-            end
+            v.Buf.Cursor.CurSelection[1].Y = sel[1].Y
+            v.Buf.Cursor.CurSelection[2].Y = sel[2].Y
+            v.Buf.Cursor.CurSelection[1].X = sel[1].X
+            v.Buf.Cursor.CurSelection[2].X = sel[2].X
         else
-            v.Buf.Cursor.X = v.Buf.Cursor.X - index
+            v.Buf.Cursor.X = curpos.X - index
+            v.Buf.Cursor.Y = curpos.Y
         end
     else
         local commentedLine = commentType:gsub("%%s", trim(line))
         v.Buf:Replace(Loc(0, lineN), Loc(#line, lineN), GetLeadingWhitespace(line) .. commentedLine)
         if v.Buf.Cursor:HasSelection() then
-            if v.Buf.Cursor.CurSelection[1].Y == v.Buf.Cursor.CurSelection[2].Y then
-                v.Buf.Cursor.CurSelection[1].X = v.Buf.Cursor.CurSelection[1].X + index
-                v.Buf.Cursor.CurSelection[2].X = v.Buf.Cursor.CurSelection[2].X + index
-            end
+            v.Buf.Cursor.CurSelection[1].Y = sel[1].Y
+            v.Buf.Cursor.CurSelection[2].Y = sel[2].Y
+            v.Buf.Cursor.CurSelection[1].X = sel[1].X
+            v.Buf.Cursor.CurSelection[2].X = sel[2].X
         else
-            v.Buf.Cursor.X = v.Buf.Cursor.X + index
+            v.Buf.Cursor.X = curpos.X + index
+            v.Buf.Cursor.Y = curpos.Y
         end
     end
     v.Cursor:Relocate()
@@ -70,9 +74,17 @@ function comment()
     local v = CurView()
     if v.Cursor:HasSelection() then
         if v.Cursor.CurSelection[1]:GreaterThan(-v.Cursor.CurSelection[2]) then
-            commentSelection(v.Cursor.CurSelection[2].Y, v.Cursor.CurSelection[1].Y)
+            local endLine = v.Cursor.CurSelection[1].Y
+            if v.Cursor.CurSelection[1].X == 0 then
+                endLine = endLine - 1
+            end
+            commentSelection(v.Cursor.CurSelection[2].Y, endLine)
         else
-            commentSelection(v.Cursor.CurSelection[1].Y, v.Cursor.CurSelection[2].Y)
+            local endLine = v.Cursor.CurSelection[2].Y
+            if v.Cursor.CurSelection[2].X == 0 then
+                endLine = endLine - 1
+            end
+            commentSelection(v.Cursor.CurSelection[1].Y, endLine)
         end
     else
         commentLine(v.Cursor.Y)
