6cd427c0776d44ce7aa641ab52c37710ec04d59a
Mikael Hultgren <mikael.hultgren@consortiofashion.com>
2018-03-08 11:21:13 +0100

    Go and path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,10 +9,10 @@ not set -gq __hostname; and set -g __hostname (hostname|cut -d . -f 1|tr '[:uppe
 set -gx LC_ALL en_US.UTF-8
 set -gx EDITOR "vim"
 set -gx VISUAL "$EDITOR"
-set -gx GOPATH ~/.go
 
 test -e ~/.config/fish/config.d/$__hostname.fish; and source ~/.config/fish/config.d/$__hostname.fish
 
 ### PATH ###
 test -d $HOME/.go/bin; and set -gx PATH $HOME/.go/bin $PATH
 test -d $HOME/.bin; and set -gx PATH $HOME/.bin $PATH
+test -d $HOME/opt/bin; and set -gx PATH $HOME/opt/bin $PATH
