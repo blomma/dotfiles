4ff3667eafc0504a59e3d7c518b99ebc5fce65e5
Mikael Hultgren <blomma@gmail.com>
2018-05-01 15:08:34 +0200

    New deal neomutt

--- neomutt/.offlineimap.py
+++ neomutt/.offlineimap.py
@@ -1,8 +1,10 @@
 import subprocess
 import os.path
 
-def imappasswd():
+def imappasswd(account_name):
     path = os.path.expanduser('~/.offlineimap.password')
+    if(account_name == "brf"):
+        path = os.path.expanduser('~/.offlineimap.brf.password')
     args = ["cat", path]
     try:
         return subprocess.check_output(args).strip()
