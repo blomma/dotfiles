e669e6d5c6601b1c963f6ebfc8cf465c8ceee45a
Mikael Hultgren <blomma@gmail.com>
2018-06-18 23:31:55 +0200

    FISH

--- fish/.config/fish/conf.d/omf.fish
+++ fish/.config/fish/conf.d/omf.fish
@@ -6,6 +6,11 @@ set -q XDG_DATA_HOME
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 
+### BOBTHEFISH
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_cmd_duration no
+set -g theme_display_git_dirty_verbose yes
+
 set -g theme_color_scheme terminal
 set -g theme_display_date no
 set -g theme_display_cmd_duration yes
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,11 +14,6 @@ test -d $HOME/.bin; and set -gx PATH $HOME/.bin $PATH
 test -d $HOME/opt/bin; and set -gx PATH $HOME/opt/bin $PATH
 test -d $HOME/.pyenv/bin; and set -gx PATH $HOME/.pyenv/bin $PATH
 
-### BOBTHEFISH
-set -g theme_display_git_ahead_verbose yes
-set -g theme_display_cmd_duration no
-set -g theme_display_git_dirty_verbose yes
-
 ### Ruby (rbenv) ###
 if command -s rbenv >/dev/null
 	status --is-interactive; and source (rbenv init -|psub)
