5f23058175bab572e9202ab0b0d2c7d9b682d3ea
Mikael Hultgren <blomma@gmail.com>
2018-06-19 18:50:29 +0200

    More FISHIO

--- fish/.config/fish/conf.d/omf.fish
+++ fish/.config/fish/conf.d/omf.fish
@@ -7,10 +7,13 @@ set -q XDG_DATA_HOME
 source $OMF_PATH/init.fish
 
 ### BOBTHEFISH
+set -g theme_display_git yes
+set -g theme_display_git_dirty yes
+set -g theme_display_git_untracked yes
 set -g theme_display_git_ahead_verbose yes
-set -g theme_display_cmd_duration no
 set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_master_branch yes
 
+set -g theme_display_cmd_duration no
 set -g theme_color_scheme terminal
 set -g theme_display_date no
-set -g theme_display_cmd_duration yes
