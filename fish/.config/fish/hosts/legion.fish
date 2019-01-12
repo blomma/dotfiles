33275d01b1e0c11b093cf3213714c439616527df
Mikael Hultgren <blomma@gmail.com>
2019-01-12 22:26:22 +0100

    Way out west

--- fish/.config/fish/hosts/legion.fish
+++ fish/.config/fish/hosts/legion.fish
@@ -1,2 +1,6 @@
 test -d /home/linuxbrew/.linuxbrew/bin; and set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 umask 022
+
+set -gx LD_LIBRARY_PATH /usr/local/lib:/usr/local/lib64
+set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig
+
