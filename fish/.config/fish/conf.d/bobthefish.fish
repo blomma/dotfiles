54b8482af5c6cef86283b7b4b70ff5e882a80085
Mikael Hultgren <blomma@gmail.com>
2019-01-15 10:35:51 +0100

    Fisher

--- fish/.config/fish/conf.d/bobthefish.fish
+++ fish/.config/fish/conf.d/bobthefish.fish
@@ -1,12 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
-### BOBTHEFISH
 set -g theme_display_git yes
 set -g theme_display_git_dirty yes
 set -g theme_display_git_untracked yes
