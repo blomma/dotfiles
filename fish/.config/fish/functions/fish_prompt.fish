c042ceea541300f6a81614342c22033e7114c059
Mikael Hultgren <blomma@gmail.com>
2019-01-22 12:00:33 +0100

    Still got that infection in the throat, glorious

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,10 +1,3 @@
-# name: clearance
-# ---------------
-# Based on idan. Display the following bits on the left:
-# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
-# - Current directory name
-# - Git branch and dirty state (if inside a git repo)
-
 function _git_branch_name
     echo (command git symbolic-ref --short HEAD ^/dev/null)
 end
@@ -16,38 +9,33 @@ end
 function fish_prompt
     set -l last_status $status
 
-    set -l cyan (set_color cyan)
-    set -l yellow (set_color yellow)
-    set -l red (set_color red)
-    set -l blue (set_color blue)
-    set -l green (set_color green)
     set -l normal (set_color normal)
 
-    set -l cwd $blue(pwd | string replace (echo $HOME) "~")
+    set -l blue (set_color blue)
+    set -l cwd $blue(echo $PWD | string replace (echo $HOME) "~")
 
     # Output the prompt, left to right
-
-    # Add a newline before new prompts
     echo -e ''
 
-    # Print pwd or full path
     echo -n -s $cwd $normal
 
     # Show git branch and status
-    if [ (_git_branch_name) ]
-        set -l git_branch (_git_branch_name)
-
+    set -l git_branch (_git_branch_name)
+    if test $status -eq 0
         if [ (_git_is_dirty) ]
+            set -l yellow (set_color yellow)
             set git_info '(' $yellow $git_branch "±" $normal ')'
         else
+            set -l green (set_color green)
             set git_info '(' $green $git_branch $normal ')'
         end
         echo -n -s ' · ' $git_info $normal
     end
 
-    set -l prompt_color $red
-    if test $last_status = 0
-        set prompt_color $normal
+    set -l prompt_color $normal
+    if test $last_status -ne 0
+        set -l red (set_color red)
+        set prompt_color $red
     end
 
     # Terminate with a nice prompt char
