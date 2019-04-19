ef6f30d8e61be93d94a03c8e17dbd4eca8fbb309
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-04-19 17:33:10 +0200

    Saga

--- fish/.config/fish/hosts/legion.fish
+++ fish/.config/fish/hosts/legion.fish
@@ -1,3 +1,5 @@
 set -gx LD_LIBRARY_PATH /usr/local/lib:/usr/local/lib64
 set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig
 set -x PATH /snap/bin $PATH
+
+kitty + complete setup fish | source
