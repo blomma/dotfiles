a72edbaf204f1a25e8e159c90c797b30fcd321f4
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-05-01 20:56:21 +0200

    fish

--- sway/.config/sway/sway_bg.fish
+++ sway/.config/sway/sway_bg.fish
@@ -1,4 +1,4 @@
-#!/usr/bin/env /home/linuxbrew/.linuxbrew/bin/fish
+#!/usr/bin/env /usr/bin/fish
 
 set -l bg 'output * bg' (random choice $HOME/.dotfiles/wallpaper/**.*) 'fill'
 /usr/local/bin/swaymsg $bg
