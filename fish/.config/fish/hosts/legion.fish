114c8118dd70a2ded0bf05a1f0e3a534c61bcd6e
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-05-21 17:46:15 +0200

    More living

--- fish/.config/fish/hosts/legion.fish
+++ fish/.config/fish/hosts/legion.fish
@@ -2,4 +2,4 @@ set -gx LD_LIBRARY_PATH /usr/local/lib:/usr/local/lib64
 set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig
 set -x PATH /snap/bin $PATH
 
-kitty + complete setup fish | source
+# kitty + complete setup fish | source
