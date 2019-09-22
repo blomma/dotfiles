0003b509ef23d3c88c206e52dee076d5bcedefbe
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-09-22 22:27:53 +0200

    Ohny

--- fish/.config/fish/hosts/legion.fish
+++ fish/.config/fish/hosts/legion.fish
@@ -1,5 +1,3 @@
 set -gx LD_LIBRARY_PATH /usr/local/lib:/usr/local/lib64
 set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig
 set -x PATH /snap/bin $PATH
-
-# kitty + complete setup fish | source
