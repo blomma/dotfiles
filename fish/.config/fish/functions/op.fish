e845ddc2f5ab2639929b655be71ab20824ef907f
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-10-30 15:47:36 +0100

    The rest

--- fish/.config/fish/functions/op.fish
+++ fish/.config/fish/functions/op.fish
@@ -1,6 +1,10 @@
 function op
     if command op list users ^| string match -ir 'You are not currently signed in. Please run `op signin --help` for instructions|\(ERROR\)  401: Authentication required\.' > /dev/null
         set -l session_key (command op signin blomma --output=raw)
+
+        # When called python this needs to be done before setting it again
+        set -e OP_SESSION_blomma
+
         set -Ux OP_SESSION_blomma $session_key
     end
 
