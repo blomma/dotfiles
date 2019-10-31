fa03f7f931b3543c3a34df3da1572c82cbebf5e5
Mikael Hultgren <blomma@artsoftheinsane.com>
2019-10-31 08:08:27 +0100

    Mother

--- fish/.config/fish/functions/op_.fish
+++ fish/.config/fish/functions/op_.fish
@@ -1,11 +1,11 @@
 function op_ --argument item field
-    set -l jq_command (string replace -a '#field' $field '.details?.sections[].fields[]? | select(.t=="#field").v')
+    set -l jq_command (string replace -a '#field' $field '.details?.sections[]?.fields[]? | select(.t=="#field").v')
     set -l result (op get item $item| jq -r $jq_command)
 
     if string length -q $result
         echo $result
     else
-        set -l jq_command (string replace -a '#field' $field '.details?.fields[]? | select(.name=="#field").value')
+        set -l jq_command (string replace -a '#field' $field '.details?.fields[]? | select(.designation=="#field").value')
         set -l result (op get item $item| jq -r $jq_command)
         echo $result
     end
