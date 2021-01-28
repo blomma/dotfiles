4d6c0546bf812a4b4841a1142feba44b06d1492d
Mikael Hultgren <blomma@artsoftheinsane.com>
2021-01-28 19:25:09 +0100

    Trying to work around slowdown of completion

---
+++ fish/.config/fish/functions/__fish_describe_command.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/wj/76hfszl13q76zbtymb218kq80000gn/T//fish.8diFqB/__fish_describe_command.fish @ line 2
+function __fish_describe_command --description 'Command used to find descriptions for commands'
+
+end
