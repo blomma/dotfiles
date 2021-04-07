858993404f2c1656f13460c014a8abbbe80a089b
Mikael Hultgren <blomma@artsoftheinsane.com>
2021-04-07 20:51:22 +0200

    Rethinking security

--- fish/.config/fish/functions/check_tls.fish
+++ fish/.config/fish/functions/check_tls.fish
@@ -1,4 +1,6 @@
 function check_tls
-    msmtp --serverinfo --tls --tls-certcheck=off -a blomma
-    msmtp --serverinfo --tls --tls-certcheck=off -a mother
+    # msmtp --serverinfo --tls --tls-certcheck=off -a blomma
+    # msmtp --serverinfo --tls --tls-certcheck=off -a mother
+    echo "mail.runbox.com"
+    echo | openssl s_client -connect mail.runbox.com:993 &| openssl x509 -fingerprint -noout -sha512
 end
