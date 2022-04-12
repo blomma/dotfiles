97f0e21d64692849f973ff70d92cc364b98db4ab
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-04-13 01:44:27 +0200

    Aaaaand TLS

--- fish/.config/fish/functions/check_tls.fish
+++ fish/.config/fish/functions/check_tls.fish
@@ -1,5 +1,5 @@
 function check_tls
-    # msmtp --serverinfo --tls --tls-certcheck=off -a blomma
+    msmtp --serverinfo --tls --tls-certcheck=off -a blomma
     # msmtp --serverinfo --tls --tls-certcheck=off -a mother
     echo "mail.messagingengine.com"
     echo | openssl s_client -connect mail.messagingengine.com:993 &| openssl x509 -fingerprint -noout -sha512
