39963d50910455d88b6c7c2504659567fd949236
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-04-19 12:48:04 +0200

    Something for everyone

--- nvim/.config/nvim/lua/user/lsp/lsp-signature.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-signature.lua
@@ -10,7 +10,7 @@ local cfg = {
 
     bind = true, -- This is mandatory, otherwise border config won't get registered.
     -- If you want to hook lspsaga or other signature handler, pls set to false
-    doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+    -- doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
     -- set to 0 if you DO NOT want any API comments be shown
     -- This setting only take effect in insert mode, it does not affect signature help in normal
     -- mode, 10 by default
@@ -23,7 +23,7 @@ local cfg = {
     fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
     hint_enable = true, -- virtual hint enable
     hint_prefix = "🐼 ", -- Panda for parameter
-    hint_scheme = "String",
+    hint_scheme = "Comment",
     use_lspsaga = false, -- set to true if you want to use lspsaga popup
     hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
     max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
