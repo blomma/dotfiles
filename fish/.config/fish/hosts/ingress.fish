06fbe9fdc8d58488377d36ad0807baede0dabd42
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-07-22 09:04:32 +0200

    Friday morning sync

--- fish/.config/fish/hosts/ingress.fish
+++ fish/.config/fish/hosts/ingress.fish
@@ -1,4 +1,5 @@
-fish_add_path /usr/local/share/dotnet/x64/ /opt/homebrew/opt/ruby/bin /opt/homebrew/lib/ruby/gems/3.0.0/bin $HOME/go/bin $HOME/opt/bin $HOME/.local/bin $HOME/.cargo/bin /opt/homebrew/bin /opt/homebrew/opt/node@16/bin
+fish_add_path /usr/local/share/dotnet/x64 /opt/homebrew/opt/ruby/bin /opt/homebrew/lib/ruby/gems/3.0.0/bin $HOME/go/bin $HOME/opt/bin $HOME/.local/bin $HOME/.cargo/bin /opt/homebrew/bin /opt/homebrew/opt/node@16/bin
+fish_add_path /opt/homebrew/sbin
 
 set -x NODE_BINARY /opt/homebrew/opt/node@16/bin/node
 
