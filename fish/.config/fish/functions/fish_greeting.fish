3985f8ded91ee1eb69c1db0710a8da9febd28716
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-10-29 19:53:34 +0200

    Lego masters

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,11 +1,11 @@
 function fish_greeting -d "What's up, fish?"
-        echo -n "くコ:彡 "
-        set_color $fish_color_autosuggestion
-        echo -n (uname -nmsr | string lower | string replace ".local" "")
-        echo -n " "
-        command -q -s uptime
-        and uptime
-        set_color normal
+        # echo -n "くコ:彡 "
+        # set_color $fish_color_autosuggestion
+        # echo -n (uname -nmsr | string lower | string replace ".local" "")
+        # echo -n " "
+        # command -q -s uptime
+        # and uptime
+        # set_color normal
 
-        sh (random choice ~/.config/fish/color-scripts/**)
+        # sh (random choice ~/.config/fish/color-scripts/**)
 end
