6076e7dff3c78d14a0feb0c97c080f783c06b935
Mikael Hultgren <blomma@artsoftheinsane.com>
2024-07-12 20:21:05 +0200

    Yarntastic

--- .config/nvim/lua/plugins/swift.lua
+++ .config/nvim/lua/plugins/swift.lua
@@ -1,33 +1,33 @@
--- return {
---     {
---         "nvim-treesitter/nvim-treesitter",
---         optional = true,
---         opts = function(_, opts)
---             if opts.ensure_installed ~= "all" then
---                 opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "swift" })
---             end
---         end,
---     },
---     {
---         "jay-babu/mason-nvim-dap.nvim",
---         optional = true,
---         opts = function(_, opts)
---             opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
---         end,
---     },
---     {
---         "WhoIsSethDaniel/mason-tool-installer.nvim",
---         optional = true,
---         opts = function(_, opts)
---             opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
---         end,
---     },
---     {
---         "AstroNvim/astrolsp",
---         optional = true,
---         ---@type AstroLSPOpts
---         opts = {
---             servers = { "sourcekit" },
---         },
---     },
--- }
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        optional = true,
+        opts = function(_, opts)
+            if opts.ensure_installed ~= "all" then
+                opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "swift" })
+            end
+        end,
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        optional = true,
+        opts = function(_, opts)
+            opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "codelldb" })
+        end,
+    },
+    {
+        "AstroNvim/astrolsp",
+        optional = true,
+        ---@type AstroLSPOpts
+        opts = {
+            servers = { "sourcekit" },
+        },
+    },
+}
--- .config/nvim/lua/plugins/vim-mail.lua
+++ .config/nvim/lua/plugins/vim-mail.lua
@@ -1,6 +1,5 @@
 --@type LazySpec
 return {
-
     "dbeniamine/vim-mail",
     {
         url = "https://gitlab.com/dbeniamine/vim-mail",
