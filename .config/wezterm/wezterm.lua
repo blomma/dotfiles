05f03262ccf32c1f3406088de94ea0549edb3688
Mikael Hultgren <blomma@artsoftheinsane.com>
2024-09-12 00:05:59 +0200

    Merrilo

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 
-local is_darwin = wezterm.target_triple:find("darwin") ~= nil
-local is_linux = wezterm.target_triple:find("linux") ~= nil
+local is_darwin = wezterm.target_triple:find "darwin" ~= nil
+local is_linux = wezterm.target_triple:find "linux" ~= nil
 
 local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Frappe"
@@ -11,15 +11,15 @@ config.font_size = 16
 config.enable_scroll_bar = false
 
 if is_darwin then
-	config.window_padding = {
-		left = 10,
-		right = 10,
-		top = 0,
-		bottom = 0,
-	}
+    config.window_padding = {
+        left = 10,
+        right = 10,
+        top = 0,
+        bottom = 0,
+    }
 
-	config.send_composed_key_when_left_alt_is_pressed = true
-	config.send_composed_key_when_right_alt_is_pressed = true
+    config.send_composed_key_when_left_alt_is_pressed = true
+    config.send_composed_key_when_right_alt_is_pressed = true
 end
 
 config.initial_cols = 110
@@ -27,18 +27,18 @@ config.initial_rows = 34
 config.window_close_confirmation = "NeverPrompt"
 
 if is_linux then
-	config.window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	}
+    config.window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    }
 
-	config.enable_wayland = false
-	config.default_prog = { "fish" }
-	config.window_decorations = "RESIZE"
-	config.initial_cols = 100
-	config.initial_rows = 30
+    config.enable_wayland = false
+    config.default_prog = { "fish" }
+    config.window_decorations = "RESIZE"
+    config.initial_cols = 100
+    config.initial_rows = 30
 end
 
 return config
