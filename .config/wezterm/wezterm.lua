0edbadc5e98d88b50e12da47bad11b1fc3c08bfa
Mikael Hultgren <blomma@artsoftheinsane.com>
2024-12-02 18:48:04 +0100

    Borders

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,8 +12,8 @@ config.enable_scroll_bar = false
 
 if is_darwin then
     config.window_padding = {
-        left = 10,
-        right = 10,
+        left = 0,
+        right = 0,
         top = 0,
         bottom = 0,
     }
