1ad4186c04e722b8a5b32bf784ecd015cd1bde4a
Mikael Hultgren <blomma@artsoftheinsane.com>
2025-04-15 07:12:49 +0200

    A lazy tuesday morning

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,4 +1,11 @@
 ---@type LazySpec
 return {
     "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.colorscheme.bluloco-nvim" },
+    { import = "astrocommunity.colorscheme.cyberdream-nvim" },
+    { import = "astrocommunity.colorscheme.dracula-nvim" },
+    { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+    { import = "astrocommunity.colorscheme.kanagawa-paper-nvim" },
+    { import = "astrocommunity.colorscheme.melange-nvim" },
 }
