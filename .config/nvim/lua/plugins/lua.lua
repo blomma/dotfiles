d780fd63fade1a10aa2ad7a880ad92ac50e24ec5
Mikael Hultgren <blomma@artsoftheinsane.com>
2025-06-09 18:58:44 +0200

    Simplified

--- .config/nvim/lua/plugins/lua.lua
+++ .config/nvim/lua/plugins/lua.lua
@@ -5,6 +5,8 @@ local function selene_configured(path)
     ) > 0
 end
 
+local is_aarch64 = vim.loop.os_uname().machine == "aarch64"
+
 return {
     {
         "AstroNvim/astrolsp",
@@ -14,7 +16,10 @@ return {
                 lua_ls = {
                     settings = {
                         Lua = {
-                            hint = { enable = true, arrayIndex = "Disable" },
+                            hint = {
+                                enable = true,
+                                arrayIndex = "Disable",
+                            },
                         },
                     },
                 },
@@ -47,21 +52,26 @@ return {
         "jay-babu/mason-null-ls.nvim",
         optional = true,
         opts = function(_, opts)
-            opts.ensure_installed = require("astrocore").list_insert_unique(
-                opts.ensure_installed,
-                { "stylua", "selene" }
-            )
+            opts.ensure_installed =
+                require("astrocore").list_insert_unique(opts.ensure_installed, {
+                    "stylua",
+                    (not is_aarch64 and "selene") or nil,
+                })
+
             if not opts.handlers then opts.handlers = {} end
-            opts.handlers.selene = function(source_name, methods)
-                local null_ls = require "null-ls"
-                for _, method in ipairs(methods) do
-                    null_ls.register(
-                        null_ls.builtins[method][source_name].with {
-                            runtime_condition = function(params)
-                                return selene_configured(params.bufname)
-                            end,
-                        }
-                    )
+
+            if not is_aarch64 then
+                opts.handlers.selene = function(source_name, methods)
+                    local null_ls = require "null-ls"
+                    for _, method in ipairs(methods) do
+                        null_ls.register(
+                            null_ls.builtins[method][source_name].with {
+                                runtime_condition = function(params)
+                                    return selene_configured(params.bufname)
+                                end,
+                            }
+                        )
+                    end
                 end
             end
         end,
@@ -70,10 +80,12 @@ return {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
         optional = true,
         opts = function(_, opts)
-            opts.ensure_installed = require("astrocore").list_insert_unique(
-                opts.ensure_installed,
-                { "lua-language-server", "stylua", "selene" }
-            )
+            opts.ensure_installed =
+                require("astrocore").list_insert_unique(opts.ensure_installed, {
+                    "lua-language-server",
+                    "stylua",
+                    (not is_aarch64 and "selene") or nil,
+                })
         end,
     },
     {
@@ -88,17 +100,18 @@ return {
     {
         "mfussenegger/nvim-lint",
         optional = true,
-        opts = {
-            linters_by_ft = {
-                lua = { "selene" },
-            },
-            linters = {
-                selene = {
+        opts = function(_, opts)
+            if not is_aarch64 then
+                opts.linters_by_ft = {
+                    lua = { "selene" },
+                }
+                opts.linters = opts.linters or {}
+                opts.linters.selene = {
                     condition = function(ctx)
                         return selene_configured(ctx.filename)
                     end,
-                },
-            },
-        },
+                }
+            end
+        end,
     },
 }
