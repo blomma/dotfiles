d190f48b4d3715037a7c969ff8ee8609f1a33245
Mikael Hultgren <blomma@artsoftheinsane.com>
2026-04-29 07:29:25 +0200

    Scratch

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -22,6 +22,29 @@ return {
                     -- inlay_hints = false,
                 },
             },
+            picker = {
+                layout = {
+                    layout = {
+                        backdrop = false,
+                        width = 0.8,
+                        min_width = 80,
+                        height = 0.8,
+                        min_height = 30,
+                        box = "vertical",
+                        border = true,
+                        title = "{title} {live} {flags}",
+                        title_pos = "center",
+                        { win = "input", height = 1, border = "bottom" },
+                        { win = "list", border = "none" },
+                        {
+                            win = "preview",
+                            title = "{preview}",
+                            height = 0.4,
+                            border = "top",
+                        },
+                    },
+                },
+            },
         })
     end,
 }
